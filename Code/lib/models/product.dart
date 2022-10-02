8425ff086f29b5457dc6b046338e6911e81fa907
hxchoo2018 <54625282+hxchoo2018@users.noreply.github.com>
2022-10-02 13:44:15 +0800

    Update product.dart

--- Code/lib/models/product.dart
+++ Code/lib/models/product.dart
@@ -2,8 +2,11 @@ class Product {
   final int id;
   final String name;
   final String image;
+  final String description;
+  final String category;
   final double price;
   int quantity;
 
-  Product({required this.id, required this.name, required this.image, required this.price, this.quantity = 0});
+  Product(
+      {required this.id, required this.name, required this.image, required this.description, required this.category, required this.price, this.quantity = 0});
 }
